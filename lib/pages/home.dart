f74e4a14ea3bb9fc13d860a7188dcb1a08969d27
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-02 14:40:44 +0200

    create app structure

---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add Journal Entry',
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(padding: EdgeInsets.all(24.0)),
+        color: Colors.blue,
+      ),
+    );
+  }
+}
