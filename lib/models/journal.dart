348c330c0ae954c0dee9fae6b8438c2f926cc39e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-02 15:03:54 +0200

    create journal model and database utils

---
+++ lib/models/journal.dart
@@ -0,0 +1,38 @@
+class Journal {
+  String id;
+  String date;
+  String mood;
+  String note;
+
+  Journal({
+    required this.id,
+    required this.date,
+    required this.mood,
+    required this.note,
+  });
+
+  factory Journal.fromJson(Map<String, dynamic> json) => Journal(
+        id: json['id'],
+        date: json['date'],
+        mood: json['mood'],
+        note: json['note'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'date': date,
+        'mood': mood,
+        'note': note,
+      };
+}
+
+// The JournalEdit class is responsible for passing the `action` and `journal` entry between pages
+class JournalEdit {
+  String action;
+  Journal journal;
+
+  JournalEdit({
+    required this.action,
+    required this.journal,
+  });
+}
---
+++ lib/utils/database.dart
@@ -0,0 +1,65 @@
+import 'package:ch13_local_properties/models/journal.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:convert';
+
+class DatabaseFileRoutines {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/local_persistence.json');
+  }
+
+  Future<String> readJournals() async {
+    try {
+      final file = await _localFile;
+      if (!file.existsSync()) {
+        print('File does not Exist: ${file.absolute}');
+        await writeJournals('{"journals": []}');
+      }
+      // Read the file
+      String contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      print('error readJournals: $e');
+      return '';
+    }
+  }
+
+  Future<File> writeJournals(String json) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString(json);
+  }
+}
+
+// To read and parse JSON data
+Database databaseFromJson(String str) {
+  final dataFromJson = json.decode(str);
+  return Database.fromJson(dataFromJson);
+}
+
+// To save and parse to JSON data
+String databaseToJson(Database data) {
+  final dataToJson = data.toJson();
+  return json.encode(dataToJson);
+}
+
+class Database {
+  List<Journal> journals;
+
+  Database({required this.journals});
+
+  factory Database.fromJson(Map<String, dynamic> json) => Database(
+        journals: json['journals'].map((x) => Journal.fromJson(x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'journals': journals.map((x) => x.toJson()).toList(),
+      };
+}
